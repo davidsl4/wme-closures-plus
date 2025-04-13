63a751150f42759b8fb33bf7f657180deefa0ce0
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2025-04-14 00:19:01 +0300

    fix: update version number and change SDK requirement to CDN link

--- tampermonkey.dev-template.js
+++ tampermonkey.dev-template.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name WME Closures+ [DEV]
 // @description WME Closures+ extends the Waze Map Editor with advanced closure management features
-// @version 1.0.0-dev
+// @version 0.1.1
 // @author r0den
 // @match https://*.waze.com/*editor*
 // @match https://waze.com/*editor*
 // @match https://*.wazestg.com/*editor*
 // @connect localhost
 // @require file:///{{DIST}}/wme-sdk-plus.js
-// @require file:///{{DIST}}/wme-closures-plus.user.js
+// @require https://cdn.jsdelivr.net/gh/WazeSpace/wme-sdk-plus@1.1.0-alpha.1/wme-sdk-plus.js
 // @grant GM_xmlhttpRequest
 // ==/UserScript==
