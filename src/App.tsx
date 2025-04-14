22424153f8e41e7451eaba003d265dec2de0a88e
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2025-04-14 14:28:25 +0300

    chore: remove unused redundant code from the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import { DialogOutlet, ReccuringClosureConfigDialog } from 'components/dialogs';
 import { ClosureEditorPanel } from 'components/portals';
 import { WmeSdkProvider } from 'contexts/WmeSdkContext';
-import { useEffect, useRef } from 'react';
 import { WmeSDK } from 'wme-sdk-typings';
 
 interface AppProps {
   wmeSdk?: WmeSDK;
 }
 export function App(props: AppProps) {
-  const dialogOutletRef = useRef<DialogOutlet>(null);
-
-  useEffect(() => {
-    dialogOutletRef.current
-      ?.showDialog(
-        ReccuringClosureConfigDialog,
-        {},
-        {
-          dialogProps: {
-            title: 'Set Closure Repetition',
-          },
-        },
-      )
-      .then((value) => console.log('Dialog Closed', value))
-      .catch((error) => console.log('Dialog Cancelled', error));
-  }, []);
-
   return (
     <WmeSdkProvider wmeSdk={props.wmeSdk}>
       <ClosureEditorPanel />
