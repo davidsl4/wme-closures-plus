69443eae9bac26f8562693d63c560b3631c974d1
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2025-04-19 01:37:51 +0300

    fix: update wme-sdk-plus version to 1.1.0-alpha.2 in user scripts

--- tampermonkey.meta.js
+++ tampermonkey.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name WME Closures+
 // @description WME Closures+ extends the Waze Map Editor with advanced closure management features
-// @version 0.1.0-alpha.2
+// @version 0.1.0-alpha.3
 // @author r0den
 // @match https://*.waze.com/*editor*
 // @match https://waze.com/*editor*
 // @match https://*.wazestg.com/*editor*
-// @require https://cdn.jsdelivr.net/gh/WazeSpace/wme-sdk-plus@1.1.0-alpha.1/wme-sdk-plus.js
+// @require https://cdn.jsdelivr.net/gh/WazeSpace/wme-sdk-plus@1.1.0-alpha.2/wme-sdk-plus.js
 // ==/UserScript==
