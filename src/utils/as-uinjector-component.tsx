bb5a2d83685750c0665dac4ec22eb3695d69ad38
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2025-04-30 00:43:09 +0300

    refactor: enhance UInjectorComponent type definitions for better type safety

--- src/utils/as-uinjector-component.tsx
+++ src/utils/as-uinjector-component.tsx
@@ -3,23 +3,23 @@ import {
   UInjectorProps,
   UInjectorRenderUIOptions,
 } from 'components';
-import { ComponentType, FunctionComponent } from 'react';
+import { ComponentType, createElement, FunctionComponent } from 'react';
 import { getDisplayName } from './get-display-name';
 
-export type UInjectorComponentProps = UInjectorRenderUIOptions;
+export type UInjectorComponentProps<E extends Element> = UInjectorRenderUIOptions<E>;
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export function asUInjectorComponent<P extends {}>(
-  Component: ComponentType<P & UInjectorComponentProps>,
+export function asUInjectorComponent<P extends {}, E extends Element = Element>(
+  Component: ComponentType<P & UInjectorComponentProps<E>>,
   options: Omit<UInjectorProps, 'renderUI'>,
-): ComponentType<P & Partial<typeof options>> {
+): ComponentType<Omit<P, keyof UInjectorRenderUIOptions> & Partial<typeof options>> {
   const UInjectorComponent: FunctionComponent<P & typeof options> = (props) => {
     props = Object.assign({}, options, props);
 
     return (
       <UInjector
         {...props}
-        renderUI={(options) => <Component {...options} {...props} />}
+        renderUI={(options) => createElement(Component as any, { ...options, ...props })}
       />
     );
   };
