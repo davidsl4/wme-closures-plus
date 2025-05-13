8984986a74d3085f2493922e920fc5ef8a9a5005
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2025-05-13 22:37:57 +0300

    feat(stepper): define core types and interfaces for the Stepper module

---
+++ src/components/stepper/interfaces/icon-detail.ts
@@ -0,0 +1,7 @@
+export interface IconDetail {
+  /** The class name for the icon font, e.g., "icon-user" */
+  name: string;
+
+  /** The desired color, e.g., "blue", "#FF5733", "var(--some-css-variable)" */
+  color?: string;
+}
