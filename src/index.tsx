fdf82e6a55de08325608136d16b52d86a2593db6
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2025-05-20 23:55:29 +0300

    feat: change the script's tab label to include the branded "plus" sign
    Updated AppWrapper to dynamically render a branded "+" icon when the script name ends with a "+" character. Added a new SVG asset, `plus-branded.svg`, to represent the icon and applied styling for proper alignment. This enhances UI clarity by visually distinguishing scripts with a "+" suffix.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { getWmeSdk, SDK_INITIALIZED } from './utils/sdk-utils';
 import { App } from './App';
 import { asScriptTab } from 'utils/as-script-tab';
 import { createElement } from 'react';
+import PlusBranded from './assets/plus-branded.svg';
 
 import './utils/wme-date-format';
 
@@ -15,7 +16,18 @@ const wmeSdk = await initWmeSdkPlus(
 );
 
 const root = createRoot(document.createDocumentFragment());
-const AppWrapper = asScriptTab(App, __SCRIPT_SHORT_NAME__);
+const AppWrapper = asScriptTab(
+  App,
+  <>
+    {__SCRIPT_SHORT_NAME__.endsWith('+') ?
+      __SCRIPT_SHORT_NAME__.substring(0, __SCRIPT_SHORT_NAME__.length - 1)
+    : __SCRIPT_SHORT_NAME__}
+
+    <PlusBranded
+      style={{ verticalAlign: 'middle', transform: 'translateY(-10%)' }}
+    />
+  </>,
+);
 
 root.render(
   createElement(AppWrapper, {
